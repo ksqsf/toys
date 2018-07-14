5bf5c2ac6357f4b7a1256607b5eae3f7719ab084
_rika <i@ksqsf.moe>
2018-07-14 23:49:55 +0800

    debugger: dwarf toy

---
+++ debugger/work.c
@@ -0,0 +1,22 @@
+// debugging info from dwarf
+//
+// objdump --dwarf=info work
+
+#include <stdio.h>
+
+
+void do_stuff(int my_arg)
+{
+    int my_local = my_arg + 2;
+    int i;
+
+    for (i = 0; i < my_local; ++i)
+        printf("i = %d\n", i);
+}
+
+
+int main()
+{
+    do_stuff(2);
+    return 0;
+}
