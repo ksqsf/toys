b83c4102eed0029f73edd01675691d44fca0c797
_rika <i@ksqsf.moe>
2018-09-02 15:59:18 +0800

    websocketd: quadcore test
    it's fun to watch memory usage grow

--- websocketd/client.py
+++ websocketd/client.py
@@ -3,19 +3,25 @@
 # This script requires 'websocket-client'.
 from websocket import create_connection
 import time
+import threading
 
-print('connect')
-ws = create_connection("ws://127.0.0.1:54321/")
-print('connect ok')
+def test1():
+    ws = create_connection("ws://127.0.0.1:54321/")
+    print('connect ok')
 
-print('send: Hello world')
-ws.send('Hello world\n')
-print('send ok')
+    ws.send('Hello world\n')
+    print('send ok')
 
-print('recving')
-result = ws.recv()
-print('recv ok:', result[:-1])
+    result = ws.recv()
+    print('recv ok:', result[:-1])
 
-print('closing')
-ws.close()
-print('closed')
+    ws.close()
+    print('close ok')
+
+def test(num_tests):
+    for i in range(0, num_tests):
+        test1()
+
+for worker_id in range(0, 4):
+    t = threading.Thread(target=test, args=(1000,))
+    t.start()
