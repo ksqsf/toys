bffa77b62a4e9c7a273039ddc19605f3b7534ece
_rika <i@ksqsf.moe>
2018-09-02 11:50:18 +0800

    websocketd: close connections properly

--- websocketd/client.py
+++ websocketd/client.py
@@ -4,10 +4,18 @@
 from websocket import create_connection
 import time
 
+print('connect')
 ws = create_connection("ws://127.0.0.1:54321/")
+print('connect ok')
+
 print('send: Hello world')
 ws.send('Hello world\n')
 print('send ok')
+
+print('recving')
 result = ws.recv()
-print('recv:', result[:-1])
+print('recv ok:', result[:-1])
+
+print('closing')
 ws.close()
+print('closed')
--- websocketd/src/server.rs
+++ websocketd/src/server.rs
@@ -2,18 +2,22 @@
 //!
 //! The server module is responsible for all bussiness logic.
 
-use std::io;
 use std::process::{Command, Stdio};
 use futures::future::Either;
+use futures::sync::mpsc;
 use tokio;
 use tokio::prelude::*;
 use tokio::codec::Decoder;
 use tokio::codec::{BytesCodec, FramedRead, FramedWrite};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::io;
 use tokio_process::CommandExt;
+use tokio_process::{Child, ChildStdin, ChildStdout};
+use bytes::BytesMut;
+use failure::Error;
 
 use handshake::handshake;
-use streams::{self, Chunk, EncodeError};
+use streams::{self, Chunk, DecodeError, EncodeError};
 
 pub fn server(
     listener: TcpListener,
@@ -45,38 +49,24 @@ fn do_pipe(stream: TcpStream, args: &Vec<String>) -> Result<(), io::Error> {
         io::ErrorKind::BrokenPipe,
         "Cannot take child stdout"
     ))?;
-
+ 
     let conn = handshake(stream)
         .and_then(|cloneable_stream| Ok(cloneable_stream.into_inner()))
         .and_then(|stream| {
             let codec = streams::StreamingCodec::new(true).framed(stream);
             let (netout, netin) = codec.split();
 
-            let from_ws_to_child = netin
-                 .fold(FramedWrite::new(childin, BytesCodec::new()),
-                      |childin, chunk| {
-                          println!("from ws: {:?}", chunk);
-                          match chunk {
-                              Chunk::Data(data) => Either::A(childin.send(data.freeze())),
-                              _ => Either::B(future::ok(childin))
-                          }
-                      }
-                )
-                .map(|_| println!("Dropping from_ws_to_child"))
-                .map_err(|e| println!("From WS to child: {}", e));
+            // Why channel?  Because we want to handle closes
+            // properly.  Actually what we really want is a cloneable
+            // `netout`, however, unfortunately, `SplitSink` doesn't
+            // implement `Clone`.  Of course, we can go the
+            // `Arc<Mutex<T>>` way, but we will have to reimplement a
+            // bunch of traits. Yeah, I'm being a little lazy here...
+            let (tx, rx) = mpsc::unbounded();
 
-            let from_child_to_ws = FramedRead::new(childout, BytesCodec::new())
-                .map_err(EncodeError::from)
-                .fold((child, netout), |(child, netout), bytes| {
-                    println!("from child: {:?}", bytes);
-                    netout.send(Chunk::Data(bytes))
-                        .map(|netout| (child, netout))
-                })
-                .map(|_| println!("Dropping from_child_to_ws"))
-                .map_err(|e| println!("From child to WS: {}", e));
-
-            tokio::spawn(from_ws_to_child);
-            tokio::spawn(from_child_to_ws);
+            tokio::spawn(from_ws_to_child(childin, netin, tx.clone()));
+            tokio::spawn(from_child_to_tx(childout, tx));
+            tokio::spawn(from_tx_to_ws(rx, child, netout));
 
             future::ok(())
         });
@@ -85,13 +75,103 @@ fn do_pipe(stream: TcpStream, args: &Vec<String>) -> Result<(), io::Error> {
     Ok(())
 }
 
+/// Copy child output to tx.
+fn from_child_to_tx(
+    childout: ChildStdout,
+    tx: mpsc::UnboundedSender<TxMessage>
+) -> impl Future<Item = (), Error = ()>
+{
+    FramedRead::new(childout, BytesCodec::new())
+        .fold(tx, |tx, out| {
+            tx.send(TxMessage::ChildOutput(out))
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))
+        })
+        .map(|_| println!("Dropping from_child_to_tx"))
+        .map_err(|e| println!("During from_child_to_tx: {}", e))
+}
+
+/// Copy Websocket input to child.
+///
+/// When a Close frame is received, this stream reports an
+/// error to interrupt the TCP stream, and hence `childin`
+/// is dropped.
+fn from_ws_to_child(
+    childin: ChildStdin,
+    netin: impl Stream<Item = Chunk, Error = DecodeError>,
+    tx: mpsc::UnboundedSender<TxMessage>,
+) -> impl Future<Item = (), Error = ()>
+{
+    let mut tx = tx.wait();
+    netin
+        .map_err(ServerError::DecodeError)
+        .fold(FramedWrite::new(childin, BytesCodec::new()),
+              move |childin, chunk| {
+                  println!("from ws: {:?}", chunk);
+                  match chunk {
+                      Chunk::Data(data) => Either::A(
+                          childin.send(data.freeze())
+                              .map_err(ServerError::IOError)
+                      ),
+                      Chunk::Close(data) => Either::B({
+                          tx.send(TxMessage::Close(data)).unwrap();
+                          future::err(ServerError::Closed)
+                      }),
+                      _ => {
+                          println!("Warning: other types of frames ignored");
+                          Either::B(future::ok(childin))
+                      }
+                  }
+              }
+        )
+        .map(|_| println!("Dropping from_ws_to_child"))
+        .map_err(|e| println!("During from_ws_to_child: {}", e))
+}
+
+/// Process tx messages.
+///
+/// When a Close message is received, a Close frame is sent to the
+/// client.
+fn from_tx_to_ws(
+    rx: mpsc::UnboundedReceiver<TxMessage>,
+    child: Child,
+    netout: impl Sink<SinkItem = Chunk, SinkError = EncodeError>
+) -> impl Future<Item = (), Error = ()>
+{
+    rx.map_err(|()| Error::from(io::Error::new(io::ErrorKind::Other, "")))
+        .fold((child, netout), |(child, netout), msg| {
+            match msg {
+                TxMessage::ChildOutput(data) => {
+                    println!("from child: {:?}", data);
+                    netout.send(Chunk::Data(data))
+                }
+                TxMessage::Close(_) => {
+                    println!("closing");
+                    netout.send(Chunk::Close(BytesMut::from("")))
+                }
+            }.map(|netout| (child, netout))
+        })
+        .map(|_| println!("Dropping from_tx_to_ws"))
+        .map_err(|e| println!("During from_tx_to_ws: {}", e))
+}
+
+enum TxMessage {
+    ChildOutput(BytesMut),
+    Close(BytesMut)
+}
+
 #[derive(Debug, Fail)]
 enum ServerError {
     #[fail(display = "Connection closed cleanly")]
-    _Closed,
+    Closed,
 
     #[fail(display = "Encode error: {}", _0)]
-    EncodeError(EncodeError)
+    EncodeError(EncodeError),
+
+    #[fail(display = "Decode error: {}", _0)]
+    DecodeError(DecodeError),
+
+    #[fail(display = "I/O error: {}", _0)]
+    IOError(io::Error)
 }
 
 impl From<EncodeError> for ServerError {
