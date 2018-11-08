7820282768eff9f327b1efbdfd598760e29f9edc
_rika <i@ksqsf.moe>
2018-11-08 23:19:16 +0800

    [c1] c1 parser

---
+++ c1/src/main.rs
@@ -0,0 +1,18 @@
+extern crate pest;
+
+#[macro_use]
+extern crate pest_derive;
+
+use std::fs;
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "c1.pest"]
+pub struct C1Parser;
+
+fn main() {
+    let file = fs::read_to_string("test/simple.c1").expect("read file");
+    let file = C1Parser::parse(Rule::file, &file).expect("unsuccessful parse")
+        .next().unwrap();
+    println!("{:#?}", file);
+}
