a76078921326915dc5fc0e71bcff21a935a43a11
ksqsf <i@ksqsf.moe>
2019-09-19 17:36:46 +0800

    Fix websocketd

--- websocketd/src/main.rs
+++ websocketd/src/main.rs
@@ -39,6 +39,7 @@ fn main() -> Result<(), Error> {
     let local_addr = "127.0.0.1:54321".parse()?;
     let listener = TcpListener::bind(&local_addr)?;
 
+    let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Please provide a command");
     } else {
